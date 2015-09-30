53ef69d9e851141e316d2493376316dbfb5f8a47
Robin Andeer <robin.andeer@gmail.com>
2015-09-30 15:49:27 +0200

    put dependencies in requirements.txt dynamically read in to setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import codecs
 import sys
 from setuptools import find_packages, setup
 from setuptools.command.test import test as TestCommand
+
+
+def parse_reqs(req_path='./requirements.txt'):
+    """Recursively parse requirements from nested pip files."""
+    install_requires = []
+    with codecs.open(req_path, 'r') as handle:
+        # remove comments and empty lines
+        lines = (line.strip() for line in handle
+                 if line.strip() and not line.startswith('#'))
+        for line in lines:
+            # check for nested requirements files
+            if line.startswith('-r'):
+                # recursively call this function
+                install_requires += parse_reqs(req_path=line[3:])
+            else:
+                # add the line as a new requirement
+                install_requires.append(line)
+    return install_requires
 
 
 # This is a plug-in for setuptools that will invoke py.test
@@ -31,11 +50,8 @@ setup(
     author_email='{{cookiecutter.email}}',
     packages=find_packages(exclude=('tests*', 'docs', 'examples')),
     include_package_data=True,
-    install_requires=[
-        'Flask>=0.10',
-        'Flask-Script',
-        'Flask-Restful',
-    ],
+    # install requirements loaded from ``requirements.txt``
+    install_requires=parse_reqs(),
     cmdclass=dict(test=PyTest),
     zip_safe=False,
     keywords='pytest flask',
